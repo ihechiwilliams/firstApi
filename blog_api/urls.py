34cfd10974b4c041352e379efa50b25ee671634d
charles <charlesclinton2003@gmail.com>
2020-05-13 16:50:37 +0100

    re

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
@@ -17,4 +19,4 @@ urlpatterns = [
     path('', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
     # path('schema/', schema_view),
     path('swagger-docs/', schema_view),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
