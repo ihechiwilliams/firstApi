65668b69f1879169f9f146850e42a842d2c9bb49
charles <charlesclinton2003@gmail.com>
2020-05-13 17:02:30 +0100

    rey

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -142,10 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
-STATIC_ROOT = "./static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = "/static/"
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
