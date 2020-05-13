ce6a60d1f47e87b2c6de1d233f14b5b059fbca71
charles <charlesclinton2003@gmail.com>
2020-05-13 17:07:52 +0100

    rays

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -64,6 +64,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
