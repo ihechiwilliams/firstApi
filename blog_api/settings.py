aa348a395cd5c929407ae86408e8c701d692ede8
charles <charlesclinton2003@gmail.com>
2020-05-13 16:10:51 +0100

    hello

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pke_9z7i$(dkaui5whgft!hof$wit==_mfhm)z!l=x)wk+d+er'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['intense-anchorage-66417.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
